c739c75286165cefcc912d3f93641a65810944ac
Amila <155749022+Savinda1@users.noreply.github.com>
2025-08-18 01:46:03 +0530

    c

--- src/lib/api/api.js
+++ src/lib/api/api.js
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BACKEND_URL = "http://localhost:8000";
+//const BACKEND_URL = "http://localhost:8000";
 
 
-//const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const api = createApi({
   reducerPath: "api",//store ekaka wage
